3ab41ea6759cd057a6894e0df7f451ae7b3bc6cb
Damiano Di Stefano <damiano@thinkpad-x240>
2016-06-23 23:04:24 +0200

    fix identation

--- client.js
+++
@@ -1,4 +0,0 @@
-var socket = require('socket.io-client')('http://localhost:3000');
-  socket.on('connect', function(){});
-  socket.on('event', function(data){});
-  socket.on('disconnect', function(){});
